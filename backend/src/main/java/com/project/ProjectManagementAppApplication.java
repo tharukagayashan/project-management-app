084bde6bc9c3aef7590dd709db9455eb90dd6639
Dev Gayashan <tharuka.gayashan.5@gmail.com>
2025-02-24 20:11:41 +0530

    spring security implementation

---
+++ backend/src/main/java/com/project/ProjectManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/project/config/JWTConstant.java
@@ -0,0 +1,6 @@
+package com.project.config;
+
+public class JWTConstant {
+    public static  final String SECRET_KEY = "secret123$%^";
+    public static final String JWT_HEADER = "Authorization";
+}
